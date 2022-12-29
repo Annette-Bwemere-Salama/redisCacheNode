d7f6ddf3239cf7d8b971bab4ce4b6368af4ed55e
Annette Bwemere <bwemereannette@gmail.com>
2022-12-29 10:16:46 +0200

    feat: first commit

---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const axios = require("axios");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+async function fetchApiData(species) {
+    const apiResponse = await axios.get(
+        `https://www.fishwatch.gov/api/species/${species}`
+    );
+    console.log("Request sent to the API");
+    return apiResponse.data;
+}
+
+// async function getSpeciesData(req,res) {
+//     const species = req.params.species;
+//     let results;
+//     // results = await fetchApiData(species);
+// } try {
+//     results =  fetchApiData(species)
+//     if(results.length === 0){
+//         throw "API returned an empty array"
+//     }
+//     res.send({
+//         fromCache: false,
+//         data: results,
+//     });
+// } catch (error) {
+//     console.log(error);
+//     res.status(404).Send("Data unavailable");
+// }
+
+
+
+
+async function getSpeciesData(req, res) {
+        const species = req.params.species;
+        let results;
+    try {
+            results =  fetchApiData(species)
+    if(results.length === 0){
+        throw "API returned an empty array"
+    }
+    res.send({
+        fromCache: false,
+        data: results,
+    });
+    } catch (error) {
+        console.log(error);
+            res.status(404).Send("Data unavailable");  
+    }  
+}
+
+
+app.get("/fish/:species", getSpeciesData)
+app.listen(port, ()=>{
+console.log(`App listene ${port}`);
+});
+
+
+
+
+
+
