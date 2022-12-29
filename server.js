b2602941af88926d67238c2e565354c498b5d1cd
Annette Bwemere <bwemereannette@gmail.com>
2022-12-29 11:39:11 +0200

    feat : cacheData

--- server.js
+++ server.js
@@ -22,24 +22,44 @@ async function fetchApiData(species) {
 }
 
 
+async function cacheData(req,res,next){
+const species = req.params.species;
+let results;
+try {
+    const cacheResults = await redisClient.get(species);
+    if (cacheResults) {
+        results = JSON.parse(cacheResults);
+        res.send({
+            fromCache: true,
+            data: results,
+        });
+    }else{
+        next()
+    }
+} catch (error) {
+   console.log(error);
+   res.status(404); 
+}
+}
+
 async function getSpeciesData(req, res) {
         const species = req.params.species;
         let results;
-        let isCached =  false;
     try {
-        const cacheResult = await redisClient.get(species);
-        if(cacheResult){
-            isCached = true;
-            results = JSON.parse(cacheResult);
-        }else{
-        results = await fetchApiData(species)
+         results = await fetchApiData(species)
         if(results.length === 0){
             throw "API returned an empty array"
         }
-        await redisClient.set(species, JSON.stringify(results));
-        }
+        await redisClient.set(species, JSON.stringify(results),{
+        EX: 180,
+        NX: true,
+        });
+        
+        // }
     res.send({
-        fromCache: isCached,
+        fromCache: 
+        false
+        ,
         data: results,
     });
     } catch (error) {
@@ -49,7 +69,7 @@ async function getSpeciesData(req, res) {
 }
 
 
-app.get("/fish/:species", getSpeciesData)
+app.get("/fish/:species",cacheData, getSpeciesData)
 app.listen(port, ()=>{
 console.log(`App listene ${port}`);
 });
